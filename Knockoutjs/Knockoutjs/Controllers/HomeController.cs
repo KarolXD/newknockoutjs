6c7770a48c9f53f64cf1b9d02770dbe288943dce
Dabrin <dabrinltecr@gmail.com>
2020-06-19 17:50:51 -0600

    Completos Delete/Update

--- Knockoutjs/Knockoutjs/Controllers/HomeController.cs
+++ Knockoutjs/Knockoutjs/Controllers/HomeController.cs
@@ -2,9 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace Knockoutjs.Controllers
 {
@@ -50,6 +52,28 @@ namespace Knockoutjs.Controllers
             return Json(metodos.listado(), JsonRequestBehavior.AllowGet);
         }
 
+
+
+        public ActionResult Update(Persona persona)
+        {
+            metodos.modificar(persona);
+            return View("Leer");
+        }
+        //GET All Courses
+        public ActionResult Edit(int id)
+        {
+            var serializer = new JavaScriptSerializer();
+            ViewBag.Persona = serializer.Serialize( metodos.selById(id));
+            return View("Update");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Persona persona = new Persona();
+            persona.id = id;
+            metodos.eliminar(persona);
+            return View("Leer");
+        }
 
     }
 }
---
+++ Knockoutjs/Knockoutjs/Scripts/ViewModels/KOdelete.js
@@ -0,0 +1,35 @@
+﻿
+$(function () {
+    ko.applyBindings(modelCreate);
+});
+
+var modelDelete = {
+
+    
+    cedula: ko.observable(),
+
+
+    SaveStudent: function () {
+
+        $.ajax({
+            url: '/Home/Create',
+            type: 'post',
+            dataType: 'json',
+            data: ko.toJSON(this),
+            contentType: 'application/json',
+            success: function (result) {
+                console.log(this.nombre);
+            },
+            error: function (err) {
+                if (err.responseText == "Creation Failed") { window.location.href = '/Home/Index/'; }
+                else {
+                    alert("Status:" + err.responseText);
+                    window.location.href = '/Home/Registrar/';
+                }
+            },
+            complete: function () {
+                window.location.href = '/Home/Registrar/';
+            }
+        });
+    }
+};
