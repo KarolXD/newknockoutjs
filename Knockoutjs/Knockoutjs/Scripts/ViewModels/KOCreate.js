56a1ae52e5897efe50aa4f6e8e2ee5f03e4065d4
Karo <karoll.university@gmail.com>
2020-06-19 20:25:12 -0600

    Dos ejemplos de Data-bing

--- Knockoutjs/Knockoutjs/Scripts/ViewModels/KOCreate.js
+++ Knockoutjs/Knockoutjs/Scripts/ViewModels/KOCreate.js
@@ -1,16 +1,17 @@
-﻿
+﻿//para activar Knockou.js por medio de un applyBindings
 $(function () {
     ko.applyBindings(modelCreate);
 });
 
+//creacion del modelo de vista modelCreate
 var modelCreate = {
 
     nombre :ko.observable(),
     apellido: ko.observable(),
     correo: ko.observable(),
     cedula:ko.observable(),
-  
-
+    myMessage: ko.observable(),
+    shouldShowMessage: ko.observable(true),
     SaveStudent: function () {
  
         $.ajax({
@@ -26,12 +27,23 @@ var modelCreate = {
                 if (err.responseText == "Creation Failed") { window.location.href = '/Home/Index/'; }
                 else {
                     alert("Status:" + err.responseText);
+                    
                     window.location.href = '/Home/Registrar/';
                 }
             },
             complete: function () {
                 window.location.href = '/Home/Registrar/';
+               
+            
             }
         });
     }
 };
+
+
+modelCreate.myMessage("Datos Requeridos");
+
+
+
+//modelCreate.shouldShowMessage(true);
+modelCreate.shouldShowMessage(false); 
